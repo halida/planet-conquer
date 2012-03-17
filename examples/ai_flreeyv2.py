89422f0c18c7b067fe80f8fc7c28dd639d3b118d
flreey <flreey@gmail.com>
2012-03-17 17:05:03 +0800

    increcement ai_flreeyv2

--- examples/ai_flreeyv2.py
+++ examples/ai_flreeyv2.py
@@ -75,12 +75,12 @@ class SimpleAI():
 
         for n, hold in enumerate(self.info['holds']):
             if hold[0] is None:
-                planets[n]['weight'] *= 2
+                planets[n]['weight'] *= 1.1
 
         #TODO: if planets near by high resources, acc it's weight
         return sorted(
                 [(n, p['weight']) for n, p in enumerate(planets)],
-                key=lambda x:x[1], reverse=True)
+                key=lambda x:x[1])
 
     def get_adjacency_planets(self, planet_ids):
         adjacency_planets = {}
@@ -91,6 +91,11 @@ class SimpleAI():
                 adjacency_planets[r[0]].append([r[1], r[2]])
         return adjacency_planets
 
+    def get_round(self, start, end):
+        for r in self.map['routes']:
+            if r[0] == start and r[1] == start:
+                return r[3]
+
     def get_best_planets(self, planets, weight):
         """
         return planets pairs, [(1, 2, 2)] means
@@ -99,11 +104,22 @@ class SimpleAI():
         adjacency_planets = self.get_adjacency_planets(planets)
         adjacency_planets_copy = adjacency_planets.copy()
         pairs = []
+        slow_down_conquer = False
+
+        if len(self.get_myplanets()) >= len(self.info['holds']):
+            slow_down_conquer = True
+
+        dangerous = self.would_be_attacked()
+        fighting_armies = []
+
         #conquer planet
         for colony_planet_id, planet_weight in weight:
             keys = adjacency_planets.keys()
             for k in keys:
                 pid = k
+                if pid in dangerous:
+                    fighting_armies.insert(0, pid)
+                    #continue
                 ad_info = adjacency_planets[k]
                 adjacency_pids = [p[0] for p in ad_info]
                 if (colony_planet_id in adjacency_pids and colony_planet_id not
@@ -113,10 +129,11 @@ class SimpleAI():
                         ad_info[adjacency_pids.index(colony_planet_id)][1]
                         ])
                     #only get once in this step
-                    #adjacency_planets.pop(pid)
+                    #if slow_down_conquer:
+                        #adjacency_planets.pop(pid)
 
         #random to reinforece armies
-        fighting_armies = [p[0] for p in pairs]
+        fighting_armies.extend([p[0] for p in pairs])
         idle_armies = set(planets).difference(fighting_armies)
 
         for idle in idle_armies:
@@ -129,7 +146,7 @@ class SimpleAI():
                 #get_nearest_planet(idle, fighting_armies)
                 #dijkstra(idle, fighting_armies
                 #TODO: get the paths to nearelest planet 
-                pairs.append([idle, adjacency_pids[0], -1])
+                pairs.append([idle, sorted(adjacency_pids, reverse=True)[0], -1])
 
         #print 'pairs', pairs
         #print 'holds', self.info['holds']
@@ -138,7 +155,9 @@ class SimpleAI():
 
     def cal_new_acount(self, current, planet, round):
         if round <= 0:
-            return current if current < planet['max'] else planet['max']
+            current if current < planet['max'] else planet['max']
+            current = current * planet['def']
+            return current
         return self.cal_new_acount(
                 int(current * planet['res'] + planet['cos']),
                 planet, round -1)
@@ -146,7 +165,6 @@ class SimpleAI():
     def move(self, pairs):
         holds = self.info['holds']
         planets = self.map['planets']
-        my_planets = self.get_myplanets()
         moves = []
         for me, anemy, round in pairs:
             my_armies = holds[me][1]
@@ -162,14 +180,19 @@ class SimpleAI():
                 if holds[anemy][0] is None:
                     #not conquer empty planets until enemy planets which is
                     #near empyt planet less than 2
-                    adjacency_planets = self.get_adjacency_planets([anemy])
-                    r = filter(lambda x:holds[x][0] is not None and x not in
-                            my_planets, [v[0] for v in adjacency_planets[anemy]])
-                    if len(r) <= 2:
-                        #TODO: change percent of 1/2 to suitable percent
-                        send_armies = my_armies * 1.0 / 2
-                    #else:
-                        #send_armies = my_armies * 2.0 / 3
+                    enemy_planets_nearby_my_planet = self.get_nearby_anemies(me)
+                    armies_less_than_my_planets = filter(lambda
+                            x:self.cal_new_acount(holds[x][1], planets[x],
+                                self.get_round(me, x)) >
+                            holds[x][1], enemy_planets_nearby_my_planet)
+                    if enemy_planets_nearby_my_planet:
+                        print 'enemy_planets_nearby_my_planet', enemy_planets_nearby_my_planet, armies_less_than_my_planets
+                        if armies_less_than_my_planets:
+                            anemy = armies_less_than_my_planets[0]
+                            send_armies = int(holds[anemy][1] + (my_armies -
+                                holds[anemy][1]) / 2.0)
+                    else:
+                        send_armies = int(my_armies * 2.0 / 3)
                 else:
                     enemy_armies = self.cal_new_acount(holds[anemy][1], planets[anemy],
                             round)
@@ -180,6 +203,32 @@ class SimpleAI():
                 holds[me][1] -= send_armies
 
         return moves
+
+    def would_be_attacked(self):
+        moves = self.info['moves']
+        my_planets = self.get_myplanets()
+        dangerous = filter(lambda x:x[0] is not self.me['seq'] and x[2] in
+                my_planets, moves)
+        #if planet weight is hight and has anemy nearby 
+        #assert it would be attacked
+        #only need top 4 planet
+        #top4 = [w[0] for w in self.cal_weight()[0:4]]
+        #warning_planets = filter(lambda x:x in my_planets, top4)
+        #s = set()
+        #[s.update(self.get_nearby_anemies(p)) for p in warning_planets]
+        #dangerous.extend(list(s))
+        return dangerous
+
+
+
+        return [d[2] for d in dangerous]
+
+    def get_nearby_anemies(self, planet):
+        ps = self.get_adjacency_planets([planet])
+        holds = self.info['holds']
+        return filter(lambda x:holds[x][0] is not self.me['seq'] and holds[x][0]
+                is not None,
+                [adinfo[0] for adinfo in ps[planet]])
 
     def get_myplanets(self):
         return [n for n, h in enumerate(self.info['holds']) if h[0] ==
